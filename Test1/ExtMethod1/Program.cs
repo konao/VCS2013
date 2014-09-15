f2592281f97e100998dd3c537008726d87c220d0
konao <konao235@gmail.com>
2014-09-15 18:44:36 +0900

    add ExtMethod1 project

---
+++ Test1/ExtMethod1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtMethod1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Extension Method");
+        }
+    }
+}
